8e8c008b9bc001d27cad034faf0d5c7b2844b241
yong liang <yongliang24@gmail.com>
2019-01-23 06:45:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,42 +1,42 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final = {}
+  hash_bird = {}
 
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
-      end 
-    end 
-  end 
-  x = final.keys
+        hash_bird[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+  x = hash_bird.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:color] << bird_color.to_s
-        end 
-      end 
-    end 
-  end 
+          hash_bird[item][:color] << bird_color.to_s
+        end
+      end
+    end
+  end
   data[:gender].each do |gender, type|
     type.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:gender] << gender.to_s
-        end 
-      end 
-    end 
-  end 
+          hash_bird[item][:gender] << gender.to_s
+        end
+      end
+    end
+  end
   data[:lives].each do |location, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:lives] << location
-        end 
-      end 
-    end 
-  end 
-  
-  return final 
+          hash_bird[item][:lives] << location
+        end
+      end
+    end
+  end
+
+  return hash_bird 
 end
